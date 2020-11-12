271440749c3042e5f4904c8bfe7cb64b11f74de3
Gabriele Falloon <ggfalloon@gmail.com>
2020-11-12 12:27:25 -0600

    function for backgroundcolor and hour

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 $(document).ready(function () {
 
-    var currentDay = document.getElementById("currentDay");
+    var currentDay = $("#currentDay");
+    var currentHour = moment().format("H");
+    var agendaItem = $("<textarea>");
+    var saveItem = $(".saveBtn");
 
-    currentDay.textContent = moment().format('llll');
+    // Display current Day
+    currentDay.text(moment().format('dddd, LL'));
 
+    // Function to determine current hour and corresponding background color
+    $(".inputBlock").each(thisHour);
 
+    function thisHour() {
+        var calHour = ($(this).attr("data-hour"));
+        calHour = parseInt(calHour);
 
+        if (calHour == currentHour) {
+            $(this).addClass("present");
+        } else if (calHour < currentHour) {
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        }
+    }
+    // Save text placed in text area to local storage
 
+    // $(saveItem).click(function () {
+    //     var store = $(this).val(agendaItem);
+    //     if (localStorage.getItem())
 
+    //         localStorage.setItem("", JSON.stringify(agendaItem))
 
+    // }
 
 
 });  
